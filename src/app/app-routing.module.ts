425bd6de11e22ba221c226ffbd7252253878ecde
Jhonatan Freitas <itjhonatan01@gmail.com>
2019-06-05 15:18:50 -0300

    protecting the route register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 import { LoginGuard } from './guards/login.guard';
+import { RegisterGuard } from './guards/register.guard';
 
 const routes: Routes = [
   {
@@ -10,7 +11,7 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard]},
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule', canActivate: [RegisterGuard] },
   { path: 'register-barber', loadChildren: './register-barber/register-barber.module#RegisterBarberPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule', canActivate: [LoginGuard]}
 
