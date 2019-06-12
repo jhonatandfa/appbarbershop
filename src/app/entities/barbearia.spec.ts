940f0ef3a406066d482f8891310e95d6a3a88a4c
Jhonatan Freitas <itjhonatan01@gmail.com>
2019-06-11 21:10:35 -0300

    generate class barbearia

---
+++ src/app/entities/barbearia.spec.ts
@@ -0,0 +1,7 @@
+import { Barbearia } from './barbearia';
+
+describe('Barbearia', () => {
+  it('should create an instance', () => {
+    expect(new Barbearia()).toBeTruthy();
+  });
+});
---
+++ src/app/entities/barbearia.ts
@@ -0,0 +1,4 @@
+export class Barbearia {
+    nome:string;
+    
+}
